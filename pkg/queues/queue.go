fd6ac26c668bba628abd5ee1448d2b900de38100
Hesam-Eskandari <eskandarihesam@gmail.com>
2021-08-15 21:42:58 -0600

    add more tests to queue

--- pkg/queues/queue.go
+++ pkg/queues/queue.go
@@ -2,12 +2,15 @@ package queues
 
 import (
 	"container/list"
-	"fmt"
+	"testing"
 )
 
 type queue struct {
-	list     *list.List
-	capacity int // zero capacity is equivalent to unlimited capacity
+	capacity                 int // zero capacity is equivalent to unlimited capacity
+	list                     *list.List
+	queueIsEmptyException    *QueueIsEmptyException
+	queueIsFullException     *QueueIsFullException
+	queueHasNilListException *QueueHasNilListException
 }
 
 type Queue interface {
@@ -18,11 +21,15 @@ type Queue interface {
 	// AppendQueue concatenates another queue to the end of primary queue
 	AppendQueue(queue Queue) (err error)
 	//Clear resets the queue to an empty queue
-	Clear() (err error)
+	Clear(capacity int) (err error)
 	// GetList returns the inner doubly linked list of the queue
 	GetList() *list.List
 	// Len returns the length of the queue
 	Len() (length int, err error)
+	// LoopAndRun loops over a compatible function from start to end
+	LoopAndRun(t *testing.T, start, end, capacity int, value func(in interface{}) (interface{}, error),
+		arr []interface{}, function func(t *testing.T, queue Queue, array []interface{}, index int, value interface{},
+			capacity int) ([]interface{}, error)) (array []interface{}, err error)
 	// Peek returns the first in of the queue
 	Peek() (value interface{}, err error)
 	// Pop removes the longest waited value (beginning) in the queue and returns its value
@@ -33,12 +40,20 @@ type Queue interface {
 	RemoveFromEnd(value interface{}, howMany int) (removed int, err error)
 	// Replace replaces the queue with another queue
 	Replace(queue Queue) (err error)
-	// Reverse reverses he queu
+	// Reverse reverses he queue
 	Reverse() (queue Queue, err error)
 	// ToArray converts the queue to an array
 	ToArray() (array []interface{}, err error)
 	// ValidateLength validates the queue is not nil and has the length of at lease one
 	ValidateLength(funcName string) (err error)
+	// 	AssertEqualArray checks if a queue and an array have the same values in the same order
+	AssertEqualArray(t *testing.T, array []interface{})
+	// AssertEqualPeek verifies the value of the peek
+	AssertEqualPeek(t *testing.T, value interface{})
+	// AssertValueAtIndex verifies the value located in an index from the end of queue
+	AssertValueAtIndex(t *testing.T, index int, value interface{})
+	// AssertError checks if an error is not nil during testing
+	AssertError(t *testing.T, err error)
 }
 
 func NewQueue(capacity ...int) Queue {
@@ -49,8 +64,11 @@ func NewQueue(capacity ...int) Queue {
 		maxLength = capacity[0]
 	}
 	return &queue{
-		list:     list.New(),
-		capacity: maxLength, // zero capacity is equivalent to unlimited capacity
+		capacity:                 maxLength, // zero capacity is equivalent to unlimited capacity
+		list:                     list.New(),
+		queueIsEmptyException:    &QueueIsEmptyException{""},
+		queueIsFullException:     &QueueIsFullException{capacity: maxLength},
+		queueHasNilListException: &QueueHasNilListException{},
 	}
 }
 
@@ -101,12 +119,13 @@ func (q *queue) AppendQueue(queue Queue) (err error) {
 }
 
 //Clear resets the queue to an empty queue
-func (q *queue) Clear() (err error) {
+func (q *queue) Clear(capacity int) (err error) {
 	// check if queue is nil
 	if _, err = q.Len(); err != nil {
 		return
 	}
 	q.list = list.New()
+	q.capacity = capacity
 	return
 }
 
@@ -118,10 +137,37 @@ func (q *queue) GetList() *list.List {
 // Len returns the length of the queue
 func (q *queue) Len() (length int, err error) {
 	if q == nil {
-		return length, fmt.Errorf("error: queue cannot be nil")
+		return length, &QueueIsNilException{}
+	}
+	if q.list == nil {
+		return length, q.queueHasNilListException
 	}
 	length = q.list.Len()
 	return
+}
+
+// LoopAndRun loops over a compatible function from start to end
+func (q *queue) LoopAndRun(t *testing.T, start, end, capacity int, value func(in interface{}) (interface{}, error),
+	arr []interface{}, function func(t *testing.T, queue Queue, array []interface{}, index int, value interface{},
+		capacity int) ([]interface{}, error)) (array []interface{}, err error) {
+	if arr == nil {
+		arr = make([]interface{}, 0, capacity)
+		if err = q.Clear(capacity); err != nil {
+			return
+		}
+	}
+	var val interface{}
+	for index := start; index < end; index++ {
+		val, err = value(index)
+		if err != nil {
+			return
+		}
+		arr, err = function(t, q, arr, index, val, capacity)
+		if err != nil {
+			return
+		}
+	}
+	return arr, nil
 }
 
 // Peek returns the first in of the queue
@@ -129,7 +175,7 @@ func (q *queue) Peek() (value interface{}, err error) {
 	if err = q.ValidateLength("Peek"); err != nil {
 		return
 	}
-	q.list.Back()
+	value = q.list.Back().Value
 	return
 }
 
@@ -228,14 +274,66 @@ func (q *queue) ValidateLength(funcName string) (err error) {
 		return
 	}
 	if length == 0 {
-		return fmt.Errorf("%s: cannot operate with queue of length zero", funcName)
+		q.queueIsEmptyException.funcName = funcName
+		return q.queueIsEmptyException
 	}
 	return
 }
 
 func (q *queue) checkCapacity() (err error) {
 	if q.capacity != 0 && q.list.Len() >= q.capacity {
-		return fmt.Errorf("reached queue capacity= %v", q.capacity)
+		return q.queueIsFullException
 	}
 	return
+}
+
+func (q *queue) AssertEqualArray(t *testing.T, array []interface{}) {
+	// validate queue
+	if err := q.ValidateLength("AssertEqualArray"); err != nil {
+		t.Errorf(err.Error())
+	}
+	for index, node := len(array)-1, q.GetList().Back(); node != nil; index-- {
+		if node.Value != array[index] {
+			queueArray, _ := q.ToArray()
+			t.Errorf("queue %v does not match to array %v", queueArray, array)
+			break
+		}
+		node = node.Prev()
+	}
+}
+
+// AssertEqualPeek verifies the value of the peek
+func (q *queue) AssertEqualPeek(t *testing.T, value interface{}) {
+	// validate queue
+	if err := q.ValidateLength("AssertEqualPeek"); err != nil {
+		t.Errorf(err.Error())
+	}
+	peek, _ := q.Peek()
+	if peek != value {
+		t.Errorf("AssertEqualPeek: peek of the queue is expected to be %v but it is equal to %v", value, peek)
+	}
+}
+
+// AssertValueAtIndex verifies the value located in an index from the end of queue
+func (q *queue) AssertValueAtIndex(t *testing.T, index int, value interface{}) {
+	// validate queue
+	if err := q.ValidateLength("AssertValueAtIndex"); err != nil {
+		t.Errorf(err.Error())
+	}
+	if index > q.list.Len()-1 {
+		t.Errorf("AssertValueAtIndex: index error: index %v should be less than queue length: %v", index, q.list.Len())
+	}
+	array, _ := q.ToArray()
+	val := array[index]
+	if val != value {
+		t.Errorf("AssertValueAtIndex: value of the queue at index %v is expected"+
+			" to be %v but it is equal to %v", index, value, val)
+	}
+}
+
+// AssertError checks if an error is not nil during testing
+func (q *queue) AssertError(t *testing.T, err error) {
+	if err != nil {
+		t.Errorf(err.Error())
+	}
 }
---
+++ pkg/queues/queue_test.go
@@ -0,0 +1,87 @@
+package queues
+
+import (
+	"fmt"
+	"math/rand"
+	"testing"
+	"time"
+)
+
+func assertEqualValue(t *testing.T, value1, value2 interface{}) {
+	if value1 != value2 {
+		t.Errorf("assertEqualValue: first value: %v and second value: %v are not equal", value1, value2)
+	}
+}
+func setupQueue(capacity, length int) (queue Queue, array []interface{}) {
+	rand.Seed(time.Now().UnixNano())
+	if capacity == -1 {
+		capacity = rand.Intn(20) + 20
+	}
+	arrayCap := length
+	if capacity != 0 && capacity < length {
+		arrayCap = rand.Intn(capacity/2) + capacity/2
+	}
+	queue = NewQueue(capacity)
+	array = make([]interface{}, 0, arrayCap)
+	for index := 0; index < arrayCap; index++ {
+		array = append(array, rand.Intn(arrayCap*2))
+	}
+	if err := queue.AppendArray(array); err != nil {
+		panic(err)
+	}
+	return
+}
+
+func TestQueue_AppendArray(t *testing.T) {
+	queue, array := setupQueue(0, 5)
+	queue.AssertEqualArray(t, array)
+
+}
+
+func TestQueue_ToArray(t *testing.T) {
+	queue, _ := setupQueue(0, 100)
+	queueArray, _ := queue.ToArray()
+	queue.AssertEqualArray(t, queueArray)
+
+}
+
+func TestQueue_Add(t *testing.T) {
+	queue, array := setupQueue(100, 80)
+	rand.Seed(time.Now().UnixNano())
+loop:
+	for index := 0; index < 50; index++ {
+		value := rand.Intn(100)
+		if err := queue.Add(value); err != nil {
+			if _, ok := err.(*QueueIsFullException); ok {
+				break loop
+			}
+			queue.AssertError(t, err)
+		}
+		array = append(array, value)
+		queue.AssertEqualArray(t, array)
+	}
+}
+
+func TestQueue_Peek(t *testing.T) {
+	queue, _ := setupQueue(50, 45)
+	rand.Seed(time.Now().UnixNano())
+	value := rand.Intn(100)
+	if err := queue.Add(value); err != nil {
+		queue.AssertError(t, err)
+	}
+	queue.AssertEqualPeek(t, value)
+}
+
+func TestQueue_Pop(t *testing.T) {
+	length := 45
+	queue, array := setupQueue(50, length)
+	for index := 0; index < length-1; index++ {
+		arr, _ := queue.ToArray()
+		fmt.Println("here1", arr)
+		value, err := queue.Pop()
+		if err != nil {
+			queue.AssertError(t, err)
+		}
+		assertEqualValue(t, value, array[index])
+	}
+}
